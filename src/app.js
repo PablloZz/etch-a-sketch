6de639e5d06654958c07c866e1225741c21e632e
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-06-15 11:10:24 +0300

    Implement ability to choose cells count
    In the index.html, add a button with the id "set-cells-amount".
    
    In the app.js, move the code for creating the game field into a
    function called createGameField to reuse this code multiple times.
    
    Assign the button mentioned above to a setCellsAmount variable and
    attach a click event listener to it. This will allow capturing user
    input and creating a new game field with a specified number of cells.
    
    Add a resize event listener to the window to recalculate the cells
    width when the window size changes.
    
    Create a function called removeCells and add it to the createGameField
    function to clear the field before appending new cells.
    
    Add an icon to the document.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const container = document.querySelector(".container");
+const setCellsAmount = document.querySelector("#set-cells-amount");
 let cellsCount = 16 ** 2;
-for (let i = 0; i < cellsCount; i++) {
-    createCell();
-}
+createGameField();
 
 container.addEventListener("mousedown", event => {
     event.target.classList.add("hover");
@@ -13,11 +12,44 @@ window.addEventListener("mouseup", event => {
     container.removeEventListener("mousemove", addHovering);
 });
 
+setCellsAmount.addEventListener("click", event => {
+    let userInput = +prompt("Input the cells amount no more than 100:", 0);
+
+    if (userInput > 100) {
+        alert("It's too much, input another number");
+        return;
+    } else if (userInput < 8) {
+        alert("It's too little, input another number");
+        return;
+    }
+
+    cellsCount = userInput ** 2;
+    createGameField();
+});
+
+window.addEventListener("resize", event => {
+    createGameField();
+});
+
 function addHovering(event) {
     if (event.target === container) {
         return;
     }
     event.target.classList.add("hover");
+}
+
+function createGameField() {
+    removeCells();
+
+    for (let i = 0; i < cellsCount; i++) {
+        createCell();
+    }
+}
+
+function removeCells() {
+    for (let i = container.children.length - 1; i >= 0; i--) {
+        container.children[i].parentElement.removeChild(container.children[i]);
+    }
 }
 
 function createCell() {
