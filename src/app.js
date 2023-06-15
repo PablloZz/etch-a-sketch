b631ddfdffd6672307d1ab685724af2129b10f83
Pavlo Zalutskiy <kyrogane1@gmail.com>
2023-06-15 09:38:43 +0300

    Add hovering effect to the cells
    Create mousedown and mousemove event listeners and attach an
    addHovering handler to them to create a hover effect.
    Add a mousedown event listener to the window to remove the
    addHovering handler. This prevents the container from being hovered
    when the mouse pointer moves out if it.
    Implement a createCell function to improve code readability.
    In the style.css, add .hover class and include user-select: none to the
    .cell class. This prevents bugs with event listeners.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const container = document.querySelector(".container");
 let cellsCount = 16 ** 2;
 for (let i = 0; i < cellsCount; i++) {
+    createCell();
+}
+
+container.addEventListener("mousedown", event => {
+    event.target.classList.add("hover");
+    container.addEventListener("mousemove", addHovering);
+});
+
+window.addEventListener("mouseup", event => {
+    container.removeEventListener("mousemove", addHovering);
+});
+
+function addHovering(event) {
+    if (event.target === container) {
+        return;
+    }
+    event.target.classList.add("hover");
+}
+
+function createCell() {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     const containerWidth = Number.parseInt(
